4750fa95b8a422004943603530d10fa0006b6418
BugHunter-X <askzenai@gmail.com>
2025-01-29 09:50:06 +0100

    Add UninitializedState.js file

---
+++ UninitializedState.js
@@ -0,0 +1,1 @@
+This error occurs when you try to access a state variable in a React Native component before it has been initialized.  This often happens when trying to access state within the component's render method before the component has finished mounting.
